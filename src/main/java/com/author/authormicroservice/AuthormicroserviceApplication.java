c210c90315b2709a5240c998b7e00b9d2f9aaa87
LorenzoElia95 <lorenzo.elia.pro@gmail.com>
2024-04-02 13:07:08 +0200

    Initial config and maven dependencies

---
+++ src/main/java/com/author/authormicroservice/AuthormicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.author.authormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthormicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthormicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/author/authormicroservice/AuthormicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.author.authormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthormicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
